34dd8476cf3b28c7a1db84c412fa907e6dfcf271
liusi2019 <liusi2019@gmail.com>
2021-10-03 19:25:44 -0700

    update COVID summary 2a

--- COVID/2a_summarize_results.R
+++ COVID/2a_summarize_results.R
@@ -137,6 +137,201 @@ write.table(df1, file="res/Fig4A_n_genes.txt", sep="\t", quote=FALSE,
             row.names = FALSE)
 
 
+# -------------------------------------------------------------------
+# read GSEA results
+# -------------------------------------------------------------------
+
+f1 <- function(x){
+  ww1 = which(x$padj < 0.05)
+  if(length(ww1) > 0){
+    x1 = x[ww1, c("pathway", "pval", "padj", "NES")]
+  }else{
+    x1 = NULL
+  }
+  x1
+}
+
+
+gesaL = list()
+
+sink("res/2_GSEA.txt")
+
+for(ct1 in cell_types){
+  
+  cat("\n---------------------------------------------------\n")
+  cat(ct1)
+  cat("\n---------------------------------------------------\n")
+  
+  gsea = readRDS(sprintf("res/1f_gsea_%s.rds", ct1))
+  gsea = gsea[methods]
+  
+  x1   = lapply(gsea, f1)
+  x1[sapply(x1, is.null)] = NULL
+  
+  if(length(x1) > 0){ print(x1) }
+  
+  gesaL[[ct1]] = gsea
+}
+sink()
+
+
+
+# -------------------------------------------------------------------
+# check GSEA results for one cell type
+# -------------------------------------------------------------------
+
+plot_gsea <- function(gseaL, ct1){
+  # ct1 = "CD8+Tcells_1"
+  DESeq2    = gesaL[[ct1]]$DESeq2
+  IDEAS_NB  = gesaL[[ct1]]$PS_nb_Was
+  IDEAS_DCA = gesaL[[ct1]]$PS_dca_direct_Was
+  
+  dim(DESeq2)
+  dim(IDEAS_NB)
+  dim(IDEAS_DCA)
+  
+  pathways1 = intersect(DESeq2$pathway, IDEAS_NB$pathway)
+  pathways2 = intersect(DESeq2$pathway, IDEAS_DCA$pathway)
+  pathways = sort(union(pathways1, pathways2))
+  length(pathways)
+  
+  mat1 = match(pathways, DESeq2$pathway)
+  mat2 = match(pathways, IDEAS_NB$pathway)
+  mat3 = match(pathways, IDEAS_DCA$pathway)
+  
+  gd = data.frame(pathway = pathways, 
+                  DESeq2 = -log10(DESeq2$pval[mat1]),
+                  IDEAS_NB = -log10(IDEAS_NB$pval[mat2]),
+                  IDEAS_DCA = -log10(IDEAS_DCA$pval[mat3]))
+  
+  g1 = ggplot(gd, aes(x=DESeq2, y=IDEAS_NB)) + geom_point(alpha = 0.5)
+  g2 = ggplot(gd, aes(x=DESeq2, y=IDEAS_DCA)) + geom_point(alpha = 0.5)
+  
+  pdf(sprintf("figures/2_GSEA_p_val_compare_%s.pdf", ct1), 
+      width=6, height=3)
+  print(ggarrange(g1, g2, ncol = 2, nrow = 1))
+  dev.off()
+}
+
+plot_gsea(gseaL, "CD8+Tcells_1")
+#plot_gsea(gseaL, "PFC_Microglia")
+
+# -------------------------------------------------------------------
+# plot GSEA results for CD8+Tcells_1
+# -------------------------------------------------------------------
+
+ct1 = "CD8+Tcells_1"
+
+#IDEAS_DCA = gesaL[[ct1]]$PS_dca_direct_Was
+#summary(IDEAS_DCA$padj)
+#IDEAS_DCA = IDEAS_DCA[which(IDEAS_DCA$padj < 0.05),]
+#IDEAS_DCA$pathway = gsub("REACTOME_", "", IDEAS_DCA$pathway)
+
+#df_gsea = as.data.frame(IDEAS_DCA)
+#df_gsea = df_gsea[order(df_gsea$pval),]
+#df_gsea$pathway = factor(df_gsea$pathway, levels=rev(df_gsea$pathway))
+#p1 = ggplot(data=df_gsea, aes(x=pathway, y=-log10(pval))) +
+#  geom_bar(stat="identity", fill="#56B4E9") + coord_flip() +
+#  theme(legend.position = "none") + xlab("")
+
+#ggsave("figures/2_GSEA_CD8+Tcells_1_IDEAS_DCA.pdf", p1, width=6, height=3)
+
+for (i in 1:length(methods)){
+  m = methods[i]
+  cur_method = gesaL[[ct1]][[i]]
+  cur_padj = cur_method$padj
+  cat(paste(m, "  minimal adjusted pvalue: ",
+            as.character(min(cur_padj, na.rm = TRUE)), "\n", sep = ""))
+  
+  row_sign_padj = which(cur_padj < 0.05)
+  cat(paste("number of pathways with adjusted pvalue<0.05: ", 
+            as.character(length(row_sign_padj)), "\n" ))
+
+  if (length(row_sign_padj)>0){
+    
+    cur_method = cur_method[row_sign_padj,]
+    cur_method$pathway = gsub("REACTOME_", "", cur_method$pathway)
+    
+    df_gsea = as.data.frame(cur_method)
+    df_gsea = df_gsea[order(df_gsea$pval),]
+    df_gsea$pathway = factor(df_gsea$pathway, levels=rev(df_gsea$pathway))
+    p1 = ggplot(data=df_gsea, aes(x=pathway, y=-log10(pval))) +
+      geom_bar(stat="identity", fill="#56B4E9") + coord_flip() +
+      theme(legend.position = "none") + xlab("")
+    
+    cur_figurename = 
+      paste("figures/2_GSEA_CD8+Tcells_1_", m, ".pdf", sep = "")
+    ggsave(cur_figurename, p1, device = "pdf", width=12, 
+           height=round(length(row_sign_padj)/4)+1)
+  }
+}
+
+
+
+
+# -------------------------------------------------------------------
+# scatter plot
+# -------------------------------------------------------------------
+
+pdf("figures/2_compare_pval_DESeq2_vs_IDEAS.pdf", width=4.5, height=4.5)
+
+for(ct1 in cell_types){
+  df1 = pv_list[[ct1]][,c("gene", "DESeq2", "PS_nb_Was", "PS_dca_direct_Was")]
+  
+  nms = c("DESeq2", "PS_nb_Was", "PS_dca_direct_Was")
+  fun1 = function(x){-log10(x)}
+  df2  = df1[, lapply(.SD, fun1), .SDcols = nms]
+  df2[,gene:=df1$gene]
+  setnames(df2, c("PS_nb_Was", "PS_dca_direct_Was"), 
+           c("IDEAS_NB", "IDEAS_DCA"))
+  
+  df2[,label_NB:=""]
+  df2[DESeq2 > 3 & IDEAS_NB < 1, label_NB:=gene]
+  df2[DESeq2 < 1 & IDEAS_NB > 3, label_NB:=gene]
+  
+  df2[,color_NB:="g11"]
+  df2[DESeq2 <= 2 & IDEAS_NB <= 2,  color_NB:="g00"]
+  df2[DESeq2 > 3 & IDEAS_NB < 1, color_NB:="g01"]
+  df2[DESeq2 < 1 & IDEAS_NB > 3, color_NB:="g01"]
+  
+  df2[,label_DCA:=""]
+  df2[DESeq2 > 3 & IDEAS_DCA < 1, label_DCA:=gene]
+  df2[DESeq2 < 1 & IDEAS_DCA > 3, label_DCA:=gene]
+  
+  df2[,color_DCA:="g11"]
+  df2[DESeq2 <= 2 & IDEAS_DCA <= 2,  color_DCA:="g00"]
+  df2[DESeq2 > 3 & IDEAS_DCA < 1, color_DCA:="g01"]
+  df2[DESeq2 < 1 & IDEAS_DCA > 3, color_DCA:="g01"]
+  
+  df2$color_NB = factor(df2$color_NB, levels = c("g00", "g11", "g01", "g10"))
+  df2$color_DCA = factor(df2$color_DCA, levels = c("g00", "g11", "g01", "g10"))
+  
+  g_NB = ggplot(df2, aes(x=DESeq2, y=IDEAS_NB, col=color_NB)) + 
+    geom_point(size = 0.4) +
+    scale_color_manual(values=c("#CCCCCC", "black", "#FC4E07", "#FC4E07")) +
+    theme(legend.position = "none") + ggtitle(ct1)
+  
+  g_NB = g_NB + geom_text_repel(aes(label = df2$label_NB), size = 3.5, 
+                                min.segment.length = 0, 
+                                box.padding = unit(0.25, "lines"),
+                                point.padding = unit(0.25, "lines")) 
+  
+  g_DCA = ggplot(df2, aes(x=DESeq2, y=IDEAS_DCA, col=color_DCA)) + 
+    geom_point(size = 0.4) +  
+    scale_color_manual(values=c("#CCCCCC", "black", "#FC4E07", "#FC4E07")) +
+    theme(legend.position = "none") + ggtitle(ct1)
+  
+  g_DCA = g_DCA + geom_text_repel(aes(label = df2$label_DCA), size = 3.5, 
+                                  min.segment.length = 0, 
+                                  box.padding = unit(0.25, "lines"),
+                                  point.padding = unit(0.25, "lines"))
+  
+  print(g_NB)
+  print(g_DCA)
+  
+}
+
+dev.off()
 
 
 
